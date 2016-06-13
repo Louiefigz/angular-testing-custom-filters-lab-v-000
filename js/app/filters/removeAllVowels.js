48eaa070140a5d79e35a26eeddec925a61d7016c
Louiefigz <figueroa.aaron@gmail.com>
2016-06-13 16:21:25 -0400

    Done.

--- js/app/filters/removeAllVowels.js
+++ js/app/filters/removeAllVowels.js
@@ -2,6 +2,27 @@ function removeAllVowels() {
 	return function (str) {
 		return str.replace(/[aeiou]/gi, '');
 	};
+
+	it('should filter and replace all vowels ', function () {
+		var mockedList = [{
+			name: 'Test',
+			removeAllVowels: 'Bread'
+		}, {
+			name: 'Test 2',
+			removeAllVowels: 'Apples'
+		}, {
+			name: 'Test 3',
+			removeAllVowels: 'Carrots'
+		}];
+
+		var results = $filter('favoriteFood')(mockedList, 'Apples');
+
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('ppls');
+	});
+
+
+
 }
 
 angular
